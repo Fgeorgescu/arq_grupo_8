756462e123b7635e4a60904c30de849a6005095c
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-04-14 11:47:13 -0300

    remove tags

--- stacks/test/infracost_test.tf
+++ stacks/test/infracost_test.tf
@@ -29,8 +29,5 @@ resource "aws_lambda_function" "my_hello_world" {
   role = "arn:aws:ec2:us-east-1:123123123123:instance/i-1231231231"
 
   memory_size = 512
-  tags = {
-    Environment = "Prod"
-  }
 }
 
