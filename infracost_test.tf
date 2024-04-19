0bfdd82365011565e5c63cd1951fff33fe2bfc28
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-04-18 21:35:12 -0300

    test

--- infracost_test.tf
+++ infracost_test.tf
@@ -21,6 +21,21 @@ resource "aws_instance" "my_web_app" {
   }
 }
 
+resource "aws_instance" "my_web_app_2" {
+  ami = "ami-005e54dee72cc1d00"
+
+  instance_type = "m3.xlarge" # <<<<<<<<<< Try changing this to m5.xlarge to compare the costs
+
+  tags = {
+    Environment = "production"
+    Service = "web-app"
+  }
+
+  root_block_device {
+    volume_size = 1000 # <<<<<<<<<< Try adding volume_type="gp3" to compare costs
+  }
+}
+
 resource "aws_lambda_function" "my_hello_world" {
   runtime = "nodejs12.x"
   handler = "exports.test"
