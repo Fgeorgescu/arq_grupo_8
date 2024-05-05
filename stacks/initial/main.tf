eb31cb1819a27426788144a7f9b6196a22d79be0
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-05 16:19:22 -0300

    Name example instance

--- stacks/initial/main.tf
+++ stacks/initial/main.tf
@@ -1,5 +1,8 @@
 resource "aws_instance" "instance-1" {
   ami = "ami-0ee3dd41c47751fe6"
   instance_type = "t2.micro"
-  name = "test-terramate"
+
+  tags = {
+    Name = "terramate-example"
+  }
 }
