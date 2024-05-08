537a74f6eafd0e6bbca969b7378c36fe02260dc4
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-07 22:11:58 -0300

    Add subnet

--- modules/master/main.tf
+++ modules/master/main.tf
@@ -1,4 +1,13 @@
 
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
 resource "aws_instance" "test_module_instance" {
   ami = "ami-07caf09b362be10b8" # Amazon Linux
   instance_type = "t2.micro"
