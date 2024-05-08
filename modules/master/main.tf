4be0c7425c9b0dd77f16832a3cb2ad0f20962fdf
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-07 23:55:54 -0300

    add sg and ingress rule

--- modules/master/main.tf
+++ modules/master/main.tf
@@ -16,3 +16,19 @@ resource "aws_instance" "test_module_instance" {
     Name = "${var.client_name}_master_instance"
   }
 }
+
+resource "aws_security_group" "allow_internet_traffic" {
+  name        = "${var.client_name}_allow_internet_traffic"
+  description = "Allow inbound traffic and all outbound traffic to the master of ${var.client_name}"
+  vpc_id      = "vpc-08c07fc88ec42d090"
+
+  tags = {
+    Name = "${var.client_name}_allow_internet_traffic"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_internet_traffic_global" {
+  security_group_id = aws_security_group.allow_internet_traffic.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"
+}
