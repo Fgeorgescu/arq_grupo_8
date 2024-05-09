8809a2a4649e3177691ca32e6af34fb0940ef950
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-09 19:07:01 -0300

    change sg naming

--- stacks/original/main.tf
+++ stacks/original/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "master_sg" {
+resource "aws_security_group" "master_sg_original" {
   name        = "original_implementation_master_sg"
   description = "Allow inbound traffic and all outbound traffic to the master of original_implementation"
   vpc_id      = "vpc-07d4e8828e405ba5b"
@@ -9,7 +9,7 @@ resource "aws_security_group" "master_sg" {
   }
 }
 
-resource "aws_security_group" "slave_sg" {
+resource "aws_security_group" "slave_sg_original" {
   name        = "original_implementation_slave_sg"
   description = "Segurity group for the slave structure of the client original_implementation"
   vpc_id      = "vpc-07d4e8828e405ba5b"
@@ -35,7 +35,7 @@ resource "aws_instance" "master_instance" {
   instance_type = "t2.large"
   subnet_id     = aws_subnet.original_subnet.id
 
-  security_groups = [ aws_security_group.master_sg.id ]
+  security_groups = [ aws_security_group.master_sg_original.id ]
   
   tags = {
     Name = "original_implementation_master_instance"
@@ -44,14 +44,14 @@ resource "aws_instance" "master_instance" {
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_internet_traffic_global" {
-  security_group_id = aws_security_group.master_sg.id
+  security_group_id = aws_security_group.master_sg_original.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "allow_slave_ingress" {
-  security_group_id = aws_security_group.master_sg.id
-  referenced_security_group_id = aws_security_group.slave_sg.id
+  security_group_id = aws_security_group.master_sg_original.id
+  referenced_security_group_id = aws_security_group.slave_sg_original.id
   ip_protocol       = "-1"
 }
 
@@ -59,20 +59,20 @@ resource "aws_vpc_security_group_ingress_rule" "allow_slave_ingress" {
 
 resource "aws_vpc_security_group_egress_rule" "allow_slave_egress" {
   security_group_id = aws_security_group.master_sg.id
-  referenced_security_group_id = aws_security_group.slave_sg.id
+  referenced_security_group_id = aws_security_group.slave_sg_original.id
   ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_ingress_rule" "ingress_from_master_sg" {
-  security_group_id = aws_security_group.slave_sg.id
-  referenced_security_group_id = aws_security_group.master_sg.id
+  security_group_id = aws_security_group.slave_sg_original.id
+  referenced_security_group_id = aws_security_group.master_sg_original.id
   description = "Ingress rule that allow the slave to communicate with the master instance"
   ip_protocol       = "-1"
 }
 
 resource "aws_vpc_security_group_egress_rule" "egress_from_master_sg" {
-  security_group_id = aws_security_group.slave_sg.id
-  referenced_security_group_id = aws_security_group.master_sg.id
+  security_group_id = aws_security_group.slave_sg_original.id
+  referenced_security_group_id = aws_security_group.master_sg_original.id
   description = "Egress rule that allow the slave to communicate with the master instance"
   ip_protocol       = "-1"
 }
@@ -83,7 +83,7 @@ resource "aws_launch_template" "slave_launch_template" {
   name_prefix   = "original_implementation_template"
   image_id      = "ami-07caf09b362be10b8"
   instance_type = "t2.small"
-  vpc_security_group_ids = [aws_security_group.slave_sg.id]
+  vpc_security_group_ids = [aws_security_group.slave_sg_original.id]
 
   tag_specifications {
       resource_type = "instance"
