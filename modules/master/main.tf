710b79d9751472d58414501a16eedebc2ae3625a
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-09 09:04:41 -0300

    update components naming

--- modules/master/main.tf
+++ modules/master/main.tf
@@ -1,13 +1,14 @@
 resource "aws_subnet" "master_public_subnet" {
   vpc_id     = "vpc-08c07fc88ec42d090"
   cidr_block = var.master_cidr_block
-
+  
   tags = {
     Name = "${var.client_name}_master_subnet"
+    Client = "${var.client_name}"
   }
 }
 
-resource "aws_instance" "test_module_instance" {
+resource "aws_instance" "master_instance" {
   ami = "ami-07caf09b362be10b8" # Amazon Linux
   instance_type = var.instance_type
   subnet_id     = aws_subnet.master_public_subnet.id
@@ -16,6 +17,7 @@ resource "aws_instance" "test_module_instance" {
   
   tags = {
     Name = "${var.client_name}_master_instance"
+    Client = "${var.client_name}"
   }
 }
 
@@ -32,3 +34,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_slave_ingress" {
 }
 
 # regla de egress
+
+resource "aws_vpc_security_group_rgress_rule" "allow_slave_egress" {
+  security_group_id = var.master_sg_id
+  referenced_security_group_id = var.slave_sg_id
+  ip_protocol       = "-1"
+}
