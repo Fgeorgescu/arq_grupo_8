0805b26c9607a3d22006dd945384679108e9242c
Franco Georgescu <franco.georgescu@mercadolibre.com>
2024-05-09 12:55:41 -0300

    infracost config

--- modules/slave/main.tf
+++ modules/slave/main.tf
@@ -44,7 +44,7 @@ resource "aws_launch_template" "slave_launch_template" {
 resource "aws_autoscaling_group" "slave_asg" {
   name = "${var.client_name}_asg"
   availability_zones = ["us-east-1a", "us-east-1b"]
-  desired_capacity   = 1
+  desired_capacity   = 2
   max_size           = 5
   min_size           = 0
 
